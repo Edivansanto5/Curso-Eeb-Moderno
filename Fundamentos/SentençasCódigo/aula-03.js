a166981a88a8ed52e27a8bf625e3ef4ddf6377ae
Edivansanto5 <edivansantos1478@gmail.com>
2022-09-08 09:52:25 -0300

    finalizando aula sobre comentarios da aula-03

---
+++ Fundamentos/SentençasCódigo/aula-03.js
@@ -0,0 +1,7 @@
+
+let soma = function(x,y){   //Aqui  cria a função
+    return x + y;          // Aqui vai somar os valores e retornar
+
+};                       // Fim da função
+
+console.log(soma(4,7));  // vai chamar a função
