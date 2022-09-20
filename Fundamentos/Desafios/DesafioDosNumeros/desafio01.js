f21c843f7ec6988c666283fff45dda93ef833544
edivan <edivansantos1478@gmail.com>
2022-09-20 16:52:23 -0300

    finalizando desafios de numeros

---
+++ Fundamentos/Desafios/DesafioDosNumeros/desafio01.js
@@ -0,0 +1,19 @@
+// nesse exemplo vamos imprimir os numeros de números pares
+{
+    console.log(0);
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+
+}
+
+// nesse exemplos vamos imprimir os numeros imapares
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
---
+++ Fundamentos/Desafios/DesafioDosNumeros/desafio02.js
@@ -0,0 +1,12 @@
+let numero = 10;
+
+for(numero = 0; numero <=10; numero++){
+    if(numero % 2 == 0){
+        console.log(numero);
+    }
+}
+for(numero = 0; numero <= 10; numero++){
+    if(numero % 2 == 1){
+        console.log(numero);
+    }
+}
