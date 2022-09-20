de54024407706ba42e9c5ec4b12b546b1ddc2d80
edivan <edivansantos1478@gmail.com>
2022-09-20 18:55:39 -0300

    finalizando aula de calcular area e fazendo um nova pasta e a arquivo

---
+++ Fundamentos/Desafios/DesafioÁrea/area.js
@@ -0,0 +1,30 @@
+// para se calcular uma area de circuferencia usa se    PI * RAIO AO QUADRADO
+
+// PI * raio * raio
+// primeira forma
+let raio = 10;
+const PI = 3.141592653589793;
+let area =  PI * (raio * raio)
+
+console.log('a area é', area);
+
+// segundo teste
+
+let raio1 = 10;
+let area1 = PI * (raio ** 2)
+console.log('A área vale',area1);
+
+
+// terceiro teste
+let raio3 = 10;
+let area3 = Math.PI * (raio * raio)
+console.log(area3);
+
+// quarto teste 04
+let raio4 = 10 ** 2;
+let pi = Math.PI;
+let area4 = pi * raio4 ;
+
+console.log(area4);
+
+console.log(Math.PI);
