fbefa8f76200718e315233e15721c43d6ea44a6a
edivan <edivansantos1478@gmail.com>
2022-09-20 17:21:14 -0300

    finalizando aula de dados e atualizando arquivos

---
+++ Fundamentos/Blocos de códigos/blocoDeCodigo02.js
@@ -0,0 +1,9 @@
+{
+    let nota1 = 2.9;
+    let nota2 = 8.8;
+    let somaDasNotas = nota1 + nota2;
+    let mediaNotas = somaDasNotas / 2;
+
+    console.log('A méddia de sua nota é ', mediaNotas);
+}
+
---
+++ Fundamentos/Dados/dado01.js
@@ -0,0 +1,12 @@
+let produto = 'Notbook';
+let preco = 2500;
+let desconto = 0.5;
+let precoComDesconto = preco * desconto
+
+console.log(precoComDesconto);
+
+let precoTotal =  preco * ( 1 - desconto);
+console.log(precoTotal);
+
+
+
