44949f3aa788597036683272dfc7f8ff7a8b935a
edivan <edivansantos1478@gmail.com>
2022-09-20 16:38:54 -0300

    finalizando aulas de blocos de codigos

---
+++ Fundamentos/SentençasCódigo/blocoDeCodigo.js
@@ -0,0 +1,24 @@
+{
+    console.log('Aqui e um bloco de codigo');
+}
+
+{
+    {
+        console.log('Aqui tambem e um bloco de codigo');
+    }
+}
+{
+    {
+        {
+            {
+                {
+                    {
+                        {
+                            console.log('Aqui tambem e outro bloco de codigo');
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
