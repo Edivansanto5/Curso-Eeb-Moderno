8364942c14589800b710545127320776fcf456ae
edivan <edivansantos1478@gmail.com>
2022-09-20 18:13:04 -0300

    finalizando aula de dados03

---
+++ Fundamentos/Dados/dados03.js
@@ -0,0 +1,14 @@
+let numero = 43;
+console.log(typeof numero);
+
+let nome = 'Maria de sousa';
+console.log(typeof nome);
+
+let salario = 5.987;
+console.log(typeof salario);
+
+let sexo = true;
+console.log(typeof sexo);
+
+let verdadeiro = false;
+console.log(typeof verdadeiro);
