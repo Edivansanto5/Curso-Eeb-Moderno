25a6c46af343ded411b3b5ff27bec09a5bb124d5
edivan <edivansantos1478@gmail.com>
2022-09-21 15:17:03 -0300

    finalizando aula de tipos de number

---
+++ Tipos em javascript/numbrer02.js
@@ -0,0 +1,13 @@
+// vamos calcular uma area de circunferencia usando as bibliotecas do js
+
+const raio = 10.9;
+let area = Math.PI * Math.pow(raio,2);
+
+console.log(area);
+
+//Exemplo dois do calculo da area
+const raio2 = 7.6;
+const PI = Math.PI; // aqui vai pegar 3.1459
+let area2 = PI * Math.pow(raio2,2) // vai pegar raio * raio
+
+console.log(PI);
