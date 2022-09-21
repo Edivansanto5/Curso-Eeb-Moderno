aa7b4f474cdbefb7f7f470f33187b86317ce24ed
edivan <edivansantos1478@gmail.com>
2022-09-20 21:38:09 -0300

    finalizando aula de tipos de variaveis

---
+++ Fundamentos/Basico de Var-Let-cconst/variaveis.js
@@ -0,0 +1,12 @@
+var numero = 2;
+let nuimero02 = 6;
+const numero03 = 23;
+
+var numero = 67;
+nuimero02 = 76;
+numero03 = 67
+
+
+console.log(numero);
+console.log(nuimero02);
+console.log(numero03);
