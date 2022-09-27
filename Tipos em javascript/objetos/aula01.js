4ad7c803c8df4c4ffa0f4dd73153a42763c4202b
edivan <edivansantos1478@gmail.com>
2022-09-27 17:23:04 -0500

    finalizando aula de objetos

---
+++ Tipos em javascript/objetos/aula01.js
@@ -0,0 +1,22 @@
+const produto = {}
+
+produto.nome = 'Notbook';
+produto.preco = 5890;
+produto.desconto = 0.50;
+
+console.log(produto.nome);
+console.log(produto.preco);
+console.log(produto.desconto);
+
+// outra forma de fazer um objeto e
+
+const produto02 = {
+    nome:'Notbook',
+    preco: 5000,
+    desconto:0.50,
+    descricao:'Lenovo i10 de decima geração e com ssd de 1TB e com 32Gran'
+}
+ // acessando os atributos do objeto
+
+console.log(produto02);
+console.log(produto02.descricao);
