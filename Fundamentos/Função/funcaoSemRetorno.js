11c9d0d5b2a2aa2a7c70e52c9b92fd786eb7f8d8
edivan <edivansantos1478@gmail.com>
2022-09-27 18:49:46 -0500

    fazendo uma função onde vai soimar 4 notas e fazer a media e  vai verificar se o aluno ta aprovado ou não

--- Fundamentos/Função/funcaoSemRetorno.js
+++ Fundamentos/Função/funcaoSemRetorno.js
@@ -9,19 +9,22 @@ somarNumero(56,78)
 // outro exemplo
 function media(){
 
-    let nota1 = 5;
-    let nota2 = 7;
-    let nota3 = 3;
-    let nota04 = 9;
+    let nota1 = 4;
+    let nota2 = 3;
+    let nota3 = 8;
+    let nota04 = 1;
 
     let soma = nota1 + nota2+ nota3 + nota04;
     let mediaNota = soma/4;
+    console.log(soma);
     console.log(mediaNota);
 
-    if(mediaNota <= 7){
-        console.log('reprovado');
+    if(mediaNota > 7 ){
+        console.log('Aprovado');
+    }else if(mediaNota <= 7 & mediaNota >= 4 ){
+        console.log('Prova Final');
     }else{
-        console.log('se fudeu')
+        console.log('reprovado Direto ');
     }
 
 }
