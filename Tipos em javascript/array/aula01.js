ad0205a67039ec7136de0d4d49265ef3b959c6d2
edivan <edivansantos1478@gmail.com>
2022-09-27 17:09:25 -0500

    finalizando aula de array

---
+++ Tipos em javascript/array/aula01.js
@@ -0,0 +1,22 @@
+let notasAluno = [9.5, 5.9, 7.9, 8.5, 4.8, 3.7, 10.0, 8.9]
+console.log(notasAluno);
+
+// pegando pelo index 06
+console.log(notasAluno[6]);
+
+// adcionando a partir do index
+notasAluno[9] = 9.7;
+console.log(notasAluno);
+
+
+// percorrendo toda a lista e mostrar quantos elementos tem no array
+console.log('o array possui ',notasAluno.length,'posição')
+
+// adicionados novos valores no array
+notasAluno.push('Matheus','Silva',45577,'teste')
+console.log(notasAluno);
+
+
+console.log(notasAluno.pop());
+
+
