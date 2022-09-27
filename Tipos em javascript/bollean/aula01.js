e49ec8cf05d672f90e2c73aa8d6224382e14d17b
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-09-27 11:26:33 -0300

    finalizando aula de bolean onde quais os valores possivel de dar verdadeiro ou false

---
+++ Tipos em javascript/bollean/aula01.js
@@ -0,0 +1,23 @@
+let verdadeiro = true;
+console.log(verdadeiro);
+
+
+// tipos que são verdadeiros em JS
+
+console.log(!!true);
+console.log(!! 4);
+console.log(!!' ');
+console.log(!!'TESTE');
+console.log(!! -4);
+console.log(!! []);
+console.log(!! {});
+console.log(!! Infinity);
+
+// tipos que sao falsos
+
+console.log('os falsos');
+console.log(!! false);
+console.log(!! 0);
+console.log(!! '');
+console.log(!! NaN);
+console.log(!! undefined);
