724572d6bf28d90ba219cf0a4b7f1d97c5c7e093
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-09-27 10:56:43 -0300

    finalizando aula de templatString

---
+++ Tipos em javascript/strings/templateStrng.js
@@ -0,0 +1,8 @@
+// todos os tipos possiveis de se colocar uma string
+
+let nome = 'Maria da silva vieira de medelos';
+let idade = 34;
+
+console.log('Olá '+ nome + ' Você tem que idade? ' + idade);
+console.log('Olá' , nome , 'Qual e a sua idade?', idade);
+console.log(`Olá!!!'${nome} Qual é a sua idade? ${idade}`);
