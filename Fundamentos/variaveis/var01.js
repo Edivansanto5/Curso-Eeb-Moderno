68efaa1533ae83f3636fe5b787165d8e543edfd1
edivan <edivansantos1478@gmail.com>
2022-09-28 13:47:45 -0300

    finalizando aula de variavel do tipo var

---
+++ Fundamentos/variaveis/var01.js
@@ -0,0 +1,21 @@
+// vamos mostrar de como e uma variavel com estilo global
+
+
+{
+    {
+        {
+            {
+                {
+                    {
+                        {
+                            var texto = 'Mãe do ceu';
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+// agora vou imprimir a mensagem no console fora do seu escopo
+console.log(texto);
+
