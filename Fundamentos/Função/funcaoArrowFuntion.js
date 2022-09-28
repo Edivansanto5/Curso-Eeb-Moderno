588f88d693843116a57979cf1ffb9a4adb3a1246
edivan <edivansantos1478@gmail.com>
2022-09-27 19:21:08 -0500

    fazendo aquivo e pasta das funçaõ arrowfuntion

---
+++ Fundamentos/Função/funcaoArrowFuntion.js
@@ -0,0 +1,14 @@
+const somar = (nota1,nota2)=>{
+    console.log(nota1 + nota2);
+}
+somar(3,90)
+
+
+const somar2 = (a,b)=>{
+    return a + b;
+}
+console.log(somar2(77,33));
+
+
+const somar3 = (a,b)=> 6 + 7;
+console.log(somar3());
---
+++ Fundamentos/Função/funcaoComRetorno.js
@@ -0,0 +1,11 @@
+function soma(nota1,nota2){
+    return nota1 + nota2
+}
+console.log(soma(5,7));
+
+
+let somar = function(va1,var2,var3,var4){
+    return va1 + var2 + var3 + var4;
+
+}
+console.log(somar(2,4,2,2));
