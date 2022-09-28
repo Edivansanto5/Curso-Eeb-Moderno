6f72a321a7417702ecc6ffe3256164ce5bb56d69
edivan <edivansantos1478@gmail.com>
2022-09-28 14:42:42 -0300

    finalizando aula de laço de repetição com a var

---
+++ Fundamentos/laçoRepetçãoVAR/laco02.js
@@ -0,0 +1,7 @@
+for(var valor = 0; valor < 10; valor++){
+    console.log(valor);
+}
+
+// ou seja a proxima instrução vai comresponder o ultimo numero do laço onde for verdadeiro 
+//e faça com que saia do laço de repetção
+console.log('valor de valor =',valor);
