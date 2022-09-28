416f53c8cef028569740ded74840141fbb832a84
edivan <edivansantos1478@gmail.com>
2022-09-28 14:14:20 -0300

    criando pasta e arquivo para aula de tipos de variavel do tipo let

---
+++ Fundamentos/variaveis/usando LET/let01.js
@@ -0,0 +1,12 @@
+{
+    {
+        {
+            {
+                let numero = 234;
+                console.log(numero);
+            }
+        }
+  
+    }
+}
+// vai da erro se fosse querer imprimir fora do escopo
